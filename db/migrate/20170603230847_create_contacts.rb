6f4d066b688126518f799695d0b24c216fa44b88
一宮百合子 <ichinomiyayuriko@mba.local>
2017-08-21 18:36:48 +0900

    emailのcolumnをstringに修正

--- db/migrate/20170603230847_create_contacts.rb
+++ db/migrate/20170603230847_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
-      t.integer :email
+      t.string :email
       t.text :content
 
       t.timestamps null: false
