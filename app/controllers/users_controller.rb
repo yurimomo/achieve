dcf973b18ca919f6c676c54b0d84db486b0d048b
一宮百合子 <ichinomiyayuriko@mba.local>
2017-10-04 15:40:35 +0900

    自分をファローできないように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   	@users = User.all
-  	
+  
+ 
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ end
 
   root 'top#index'
 
- resources :users, only: [:index, :show]
+ resources :users, only: [:index, :show, :edit]
 
  resources :relationships, only: [:create, :destroy]
 
